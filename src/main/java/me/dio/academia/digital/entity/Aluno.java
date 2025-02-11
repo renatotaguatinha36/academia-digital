d6e0fe6abc5f1ec819a595ce0a8a7062fd4d7239
renatoguara2020 <renatoguara2020@gmail.com>
2025-02-11 10:03:33 -0300

    ok

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -32,6 +32,56 @@ public class Aluno {
 
   private LocalDate dataDeNascimento;
 
+
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return this.cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getBairro() {
+    return this.bairro;
+  }
+
+  public void setBairro(String bairro) {
+    this.bairro = bairro;
+  }
+
+  public LocalDate getDataDeNascimento() {
+    return this.dataDeNascimento;
+  }
+
+  public void setDataDeNascimento(LocalDate dataDeNascimento) {
+    this.dataDeNascimento = dataDeNascimento;
+  }
+
+  public List<AvaliacaoFisica> getAvaliacoes() {
+    return this.avaliacoes;
+  }
+
+  public void setAvaliacoes(List<AvaliacaoFisica> avaliacoes) {
+    this.avaliacoes = avaliacoes;
+  }
+
   @OneToMany(mappedBy = "aluno", cascade = CascadeType.REMOVE , fetch = FetchType.LAZY)
   @JsonIgnore
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
